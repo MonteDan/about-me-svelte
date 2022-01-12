fb0288ce6459a0790759a6201ac72f2486e488f3
Daniel <71315420+MonteDan@users.noreply.github.com>
2022-01-12 22:54:38 +0100

    fixen

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,10 +1,10 @@
 <nav id="nav" class={navClass}>
   <div class="logo"></div>
   <ul>
-    <li><a href={"/"}>Úvod</a></li>
-    <li><a>O mně</a></li>
-    <li><a>Výtvory</a></li>
-    <li><a>Kontakt</a></li>
+    <li><a href="/">Úvod</a></li>
+    <li><a href="/">O mně</a></li>
+    <li><a href="/">Výtvory</a></li>
+    <li><a href="/">Kontakt</a></li>
   </ul>
 </nav>
 
